76aa8371e5ff044bd9437fc7598b0a269c6d0f00
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-06 14:02:30 +0100

    Update yearly_averages.R
    remove one site test

--- ozone_extremes/yearly_averages.R
+++ ozone_extremes/yearly_averages.R
@@ -6,9 +6,6 @@ meta = importMeta(source = "aurn")
 site_list = meta$code
 
 data_all <- importAURN(site = site_list, year = 1994:2024, meta = TRUE)  
-one_site = importAURN(site = "my1", year = 2022,
-                      meta = TRUE)
-
 
 ### calculate yearly average ozone 
 
