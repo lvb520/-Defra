e4bb2ca77698888636fdfc269e707f2388e2fd04
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-06 14:26:32 +0100

    Update o3_sites_investigations.R
    counted number of sites with an ozone measurement of last 30 years

--- ozone_extremes/o3_sites_investigations.R
+++ ozone_extremes/o3_sites_investigations.R
@@ -2,3 +2,15 @@ library(tidyverse)
 
 
 data = read_rds("data/1994_2024_all_sites.rds")
+
+### number of sites with any measurement of ozone
+
+number_of_obs = data %>%
+  group_by(site) %>%
+  summarise(non_na_o3 = sum(!is.na(o3)))
+
+count_non_na_groups = number_of_obs %>%
+  filter(non_na_o3 > 0) %>%
+  tally()  
+
+print(count_non_na_groups)
