f76948b105e9bacdb764bc34ff6dd728e323fd3d
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-06 13:57:28 +0100

    Update yearly_averages.R
    all sites for last 30 years imported and yearly average by site calculated. output saved to csv

--- ozone_extremes/yearly_averages.R
+++ ozone_extremes/yearly_averages.R
@@ -1,13 +1,18 @@
 library(openair)
 library(tidyverse)
 
+meta = importMeta(source = "aurn")
+
+site_list = meta$code
+
+data_all <- importAURN(site = site_list, year = 1994:2024, meta = TRUE)  
 one_site = importAURN(site = "my1", year = 2022,
                       meta = TRUE)
 
 
 ## calculate yearly average ozone 
 
-yearly_avg = timeAverage(one_site, 
+yearly_avg = timeAverage(data_all, 
                          avg.time = "year",
                          data.thresh = 85,
                          type = "site") %>%
