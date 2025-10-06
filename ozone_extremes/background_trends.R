650b1e707aab2c4952fe6e7d3eaa569d27629f32
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-06 16:17:00 +0100

    Update background_trends.R
    theilsen plot of yearly ozone trends

--- ozone_extremes/background_trends.R
+++ ozone_extremes/background_trends.R
@@ -2,3 +2,14 @@ library(openair)
 library(tidyverse)
 
 
+data = read_rds("data/1994_2024_all_sites.rds")
+
+filtered_data = data %>%
+  filter(site_type %in% c("Urban Background", "Urban Traffic", "Rural Background"))
+
+TheilSen(filtered_data, pollutant = "o3",
+         avg.time = "year",
+         date.breaks = 6,
+         date.format = "%Y",
+         layout = c(1, 3),
+         type = "site_type")
