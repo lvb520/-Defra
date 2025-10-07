fa53226d3a48ff7fa8eec9d8e0de52e32d5d53e5
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-07 15:11:05 +0100

    Update site_coverage_from_averages.R
    calculation of no of years with >85% coverage for each pollutant

--- trend_clustering/site_coverage_from_averages.R
+++ trend_clustering/site_coverage_from_averages.R
@@ -2,3 +2,11 @@ library(tidyverse)
 library(openair)
 
 data = read_rds("data/yearly_averages_10_years.rds")
+
+
+site_numbers = data %>%
+  group_by(site) %>%
+  summarise(o3_count = sum(!is.na(o3)),
+            nox_count = sum(!is.na(nox)),
+            pm2.5_count = sum(!is.na(pm2.5)))
+            
