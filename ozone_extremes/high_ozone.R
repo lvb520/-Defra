8536ec7ad12b8ea1b5f751c21411338e937d4753
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-07 11:13:38 +0100

    Update high_ozone.R
    switch to top 1%

--- ozone_extremes/high_ozone.R
+++ ozone_extremes/high_ozone.R
@@ -8,41 +8,37 @@ o3_concs = data %>%
   select(site, date, o3)
 
 
-cut_data = cutData(data, 
-                   type = "o3",
-                   n.levels = 10)
-
 urban_traffic = data %>%
   filter(site_type == "Urban Traffic")
 
 cut_urban_traffic = cutData(urban_traffic,
                             type = "o3",
-                            n.levels = 10) %>%
-  filter(o3 == "o3 60.1 to 328")
+                            n.levels = 100) %>%
+  filter(o3 == "o3 86 to 328")
 
 urban_background = data %>%
   filter(site_type == "Urban Background")
 
 cut_urban_background = cutData(urban_background,
                             type = "o3",
-                            n.levels = 10) %>%
-  filter(o3 == "o3 74 to 280")
+                            n.levels = 100) %>%
+  filter(o3 == "o3 104 to 280")
 
 rural_background = data %>%
   filter(site_type == "Rural Background")
 
 cut_rural_background = cutData(rural_background,
                                type = "o3",
-                               n.levels = 10) %>%
-  filter(o3 == "o3 84 to 278")
+                               n.levels = 100) %>%
+  filter(o3 == "o3 112 to 278")
 
-top_10_percent = cut_urban_traffic %>%
+top_1_percent = cut_urban_traffic %>%
   bind_rows(cut_urban_background) %>%
   bind_rows(cut_rural_background) %>%
   rename(o3_category = o3) %>%
   left_join(o3_concs)
 
-TheilSen(top_10_percent, pollutant = "o3",
+TheilSen(top_1_percent, pollutant = "o3",
          avg.time = "month",
          date.breaks = 6,
          date.format = "%Y",
