800369d5eb3ae2bc4eabbc730f8c675e2d04e4c5
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-07 15:02:22 +0100

    Create yearly averages.R
    changed script to yearly average calculation only

---
+++ trend_clustering/yearly averages.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(openair)
+
+
+data = read_rds("../global/data/1994_2024_all_sites.rds")
+
+a = selectByDate(data, 
+                 year = 2014:2024)
+
+averages = timeAverage(a,
+                  avg.time = "year",
+                  data.thresh = 85,
+                  type = "site")
+
+write_rds(averages, 
+          file = "data/yearly_averages_10_years.rds")
