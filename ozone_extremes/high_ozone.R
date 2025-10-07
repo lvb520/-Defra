31b91df492ea6b4a92ca15e711a80490b3824f11
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-07 10:43:15 +0100

    Update high_ozone.R
    merge top 10% for all site types and merge with measured concentrations

--- ozone_extremes/high_ozone.R
+++ ozone_extremes/high_ozone.R
@@ -4,6 +4,9 @@ library(tidyverse)
 
 data = read_rds("../global/data/1994_2024_all_sites.rds")
 
+o3_concs = data %>%
+  select(site, date, o3)
+
 
 cut_data = cutData(data, 
                    type = "o3",
@@ -14,18 +17,27 @@ urban_traffic = data %>%
 
 cut_urban_traffic = cutData(urban_traffic,
                             type = "o3",
-                            n.levels = 10)
+                            n.levels = 10) %>%
+  filter(o3 == "o3 60.1 to 328")
 
 urban_background = data %>%
   filter(site_type == "Urban Background")
 
 cut_urban_background = cutData(urban_background,
                             type = "o3",
-                            n.levels = 10)
+                            n.levels = 10) %>%
+  filter(o3 == "o3 74 to 280")
 
 rural_background = data %>%
   filter(site_type == "Rural Background")
 
 cut_rural_background = cutData(rural_background,
                                type = "o3",
-                               n.levels = 10)
+                               n.levels = 10) %>%
+  filter(o3 == "o3 84 to 278")
+
+top_10_percent = cut_urban_traffic %>%
+  bind_rows(cut_urban_background) %>%
+  bind_rows(cut_rural_background) %>%
+  rename(o3_category = o3) %>%
+  left_join(o3_concs)
