4b1430c568e6d513fb4291d494fce8822c4f69e1
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-07 15:36:46 +0100

    Update site_coverage_from_averages.R
    counted number of years for each site which have coverage > 85%. did a loop to check cutoff for how many years out of last 11 required

--- trend_clustering/site_coverage_from_averages.R
+++ trend_clustering/site_coverage_from_averages.R
@@ -9,4 +9,25 @@ site_numbers = data %>%
   summarise(o3_count = sum(!is.na(o3)),
             nox_count = sum(!is.na(nox)),
             pm2.5_count = sum(!is.na(pm2.5)))
+
+count_cols = site_numbers %>% 
+  select(ends_with("_count")) %>% 
+  names()
             
+results = map_dfr(1:10, function(threshold) {
+  n_sites <- site_numbers %>%
+    filter(if_all(all_of(count_cols), ~ . > threshold)) %>%
+    nrow()
+  
+  tibble(threshold = threshold, n_sites = n_sites)
+}) %>%
+  mutate(years_of_good_coverage = threshold + 1)
+
+
+results %>%
+  ggplot(aes(x = years_of_good_coverage, y = n_sites)) +
+  geom_point() +
+  labs(x = "years with good coverage",
+       y = "number of sites to be analysed") +
+  scale_x_continuous(n.breaks = 11) +
+  theme_bw()
