07556e20b3cead42c020ec314fa7995810bb1e39
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-07 10:06:48 +0100

    Update yearly_averages.R
    removed data import, reads in global rds instead

--- ozone_extremes/yearly_averages.R
+++ ozone_extremes/yearly_averages.R
@@ -3,12 +3,7 @@ library(tidyverse)
 
 meta = importMeta(source = "aurn")
 
-site_list = meta$code
-
-data_all <- importAURN(site = site_list, year = 1994:2024, meta = TRUE)  
-
-saveRDS(data_all, 
-        file = "data/1994_2024_all_sites.rds")
+data_all = read_rds("data/1994_2024_all_sites.rds")
 
 ### calculate yearly average ozone 
 
