c36fb393ff117917d24ea20225f198b5dd1fee08
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-08 11:06:45 +0100

    Update theilsen_trends.R
    added trends for o3 and pm2.5

--- trend_clustering/theilsen_trends.R
+++ trend_clustering/theilsen_trends.R
@@ -17,3 +17,22 @@ nox_trends = TheilSen(cluster_data,
 
 nox_results = nox_trends$data[[2]] %>% # slope is trend per year, lower and upper are 95% CI of the slope
   drop_na(slope)
+
+
+o3_trends = TheilSen(cluster_data,
+                      pollutant = "o3",
+                      type = "site",
+                      avg.time = "year")
+
+o3_results = o3_trends$data[[2]] %>% # slope is trend per year, lower and upper are 95% CI of the slope
+  drop_na(slope)
+
+
+
+pm2.5_trends = TheilSen(cluster_data,
+                     pollutant = "pm2.5",
+                     type = "site",
+                     avg.time = "year")
+
+pm2.5_results = pm2.5_trends$data[[2]] %>% # slope is trend per year, lower and upper are 95% CI of the slope
+  drop_na(slope)
