cc662a784f75ba286303976c2f57d14827b48bf6
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-09 12:53:21 +0100

    Update trends_plotting.R
    add forest plot

--- trend_clustering/trends_plotting.R
+++ trend_clustering/trends_plotting.R
@@ -1,3 +1,14 @@
 library(tidyverse)
+library(openair)
 
-trends = read_csv("data/annual_trends.csv")
+meta = importMeta(source = "aurn")
+
+trends = read_csv("data/annual_trends.csv") %>%
+  left_join(meta)
+
+trends %>%
+  ggplot(aes(x = reorder(site, latitude), y = nox_slope, ymin = nox_lower, ymax = nox_upper)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 1) +
+  coord_flip() +
+  theme_minimal()
