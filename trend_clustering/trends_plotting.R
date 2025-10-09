8ddf89bd40f827f5445e0b360a3d1fb4f715bbfd
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-09 13:32:45 +0100

    Update trends_plotting.R
    pivot longer

--- trend_clustering/trends_plotting.R
+++ trend_clustering/trends_plotting.R
@@ -1,14 +1,32 @@
 library(tidyverse)
 library(openair)
+library(fuzzyjoin)
 
 meta = importMeta(source = "aurn")
+caz = read_csv("data/clean_air_zomes.csv")
 
 trends = read_csv("data/annual_trends.csv") %>%
   left_join(meta)
 
-trends %>%
-  ggplot(aes(x = reorder(site, latitude), y = nox_slope, ymin = nox_lower, ymax = nox_upper)) +
-  geom_pointrange() +
+
+trends_with_caz = regex_left_join(trends, caz, 
+                                  by = c("site" = "city"),
+                  ignore_case = TRUE)
+
+trends_long <- trends_with_caz %>%
+  pivot_longer(
+    cols = matches("_(slope|upper|lower|p.stars)$"),
+    names_to = c("pollutant", ".value"),
+    names_pattern = "(.*)_(slope|upper|lower|p.stars)"
+  )
+
+
+
+trends_with_caz %>%
+  filter(site_type == "Urban Background") %>%
+  ggplot(aes(x = reorder(site, latitude), y = o3_slope, ymin = o3_lower, ymax = o3_upper)) +
+  geom_pointrange(aes(colour = CAZ_category)) +
   geom_hline(yintercept = 1) +
   coord_flip() +
+  labs(x = "site by latitude") +
   theme_minimal()
