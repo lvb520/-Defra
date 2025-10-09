fc53c4ddec454d71d81558b0e8c724f9d936b971
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-10-09 15:15:36 +0100

    Update trends_plotting.R
    plotting by slope order e.g. when nox slope is highest, what do o3 and pm look like

--- trend_clustering/trends_plotting.R
+++ trend_clustering/trends_plotting.R
@@ -9,16 +9,26 @@ trends = read_csv("data/annual_trends.csv") %>%
   left_join(meta)
 
 
+CAZ_numbers = c(0, 1, 2, 3, 4)
+CAZ_category = c("NA", "A", "B", "C", "D")
+
+caz_numeric = data.frame(CAZ_numbers, CAZ_category)
+
+caz = caz %>%
+  left_join(caz_numeric)
+
+
 trends_with_caz = regex_left_join(trends, caz, 
                                   by = c("site" = "city"),
-                  ignore_case = TRUE)
+                                  ignore_case = TRUE)
 
 trends_long <- trends_with_caz %>%
   pivot_longer(
     cols = matches("_(slope|upper|lower|p.stars)$"),
     names_to = c("pollutant", ".value"),
     names_pattern = "(.*)_(slope|upper|lower|p.stars)"
-  )
+  ) %>%
+  mutate(CAZ_numbers = replace_na(CAZ_numbers, 0))
 
 
 
@@ -29,4 +39,27 @@ trends_with_caz %>%
   geom_hline(yintercept = 1) +
   coord_flip() +
   labs(x = "site by latitude") +
+  theme_minimal()
+
+trends_long %>%
+  filter(slope > -20) %>%
+  ggplot(aes(x = reorder(site, CAZ_numbers), y = slope, ymin = lower, ymax = upper)) +
+  geom_pointrange(aes(colour = CAZ_category)) +
+  geom_hline(yintercept = 0) +
+  coord_flip() +
+  labs(x = "site by CAZ category") +
+  facet_wrap(~pollutant, scales = "free_x") +
+  theme_minimal()
+
+
+trends_long %>%
+  left_join(trends) %>%
+  #filter(site_type == "Rural Background") %>%
+  filter(slope > -20) %>%
+  ggplot(aes(x = reorder(site, o3_slope), y = slope, ymin = lower, ymax = upper)) +
+  geom_pointrange(aes(colour = CAZ_category)) +
+  geom_hline(yintercept = 0) +
+  coord_flip() +
+  labs(x = "site ordered by latitude") +
+  facet_wrap(~pollutant, scales = "free_x") +
   theme_minimal()
