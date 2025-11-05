e11a4be9672326e6f42781467ac6a8e97a038a05
lvb520 <94913622+lvb520@users.noreply.github.com>
2025-11-05 10:11:47 +0000

    Update pm_correlation.R
    added hourly correlation

--- trend_clustering/pm_correlation.R
+++ trend_clustering/pm_correlation.R
@@ -1,4 +1,5 @@
 library(openair)
+library(tidyverse)
 
 
 data = read_rds("data/monthly_averages_10_years.rds")
@@ -16,4 +17,20 @@ test = data_wide %>%
   select(date, Aberdeen, `London N. Kensington`)
 
 corPlot(data_wide, 
+        dendrogram = TRUE)
+
+### hourly
+
+hourly_data = read_rds("C:/Users/Lucy Brown/OneDrive - University of York/Documents/DEFRA PDRA/-Defra/global/data/1994_2024_all_sites.rds")
+a = selectByDate(hourly_data, 
+                 year = 2014:2024)
+
+hourly_wide = sites_for_clustering %>%
+  left_join(hourly_data) %>%
+  select(date, site, pm2.5) %>%
+  pivot_wider(names_from = site, 
+              values_from = pm2.5)
+
+
+corPlot(hourly_wide, 
         dendrogram = TRUE)
